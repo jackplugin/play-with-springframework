f40b5796f35b6a12cf51e4649b5d46e3563b38f6
jack <jack.li.cn@outlook.com>
2024-12-01 18:17:48 +0800

    Add dependency of Spring Context

---
+++ spring-bean-lifecycle/src/main/java/fun/erudite/playwithsf/beanlifecycle/MyBean.java
@@ -0,0 +1,7 @@
+package fun.erudite.playwithsf.beanlifecycle;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBean {
+}
---
+++ spring-bean-lifecycle/src/test/java/fun/erudite/playwithsf/beanlifecycle/MyBeanTest.java
@@ -0,0 +1,17 @@
+package fun.erudite.playwithsf.beanlifecycle;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyBeanTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
