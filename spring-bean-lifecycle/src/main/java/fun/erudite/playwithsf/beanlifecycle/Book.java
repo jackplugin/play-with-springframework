7a80ff35e73f3971a61df1328a4d5e6f7c87f8fd
jack <jack.li.cn@outlook.com>
2024-12-03 15:36:36 +0800

    Add Book class with which book beans will be defined

---
+++ spring-bean-lifecycle/src/main/java/fun/erudite/playwithsf/beanlifecycle/Book.java
@@ -0,0 +1,27 @@
+package fun.erudite.playwithsf.beanlifecycle;
+
+public class Book {
+    private String title;
+    private String isbn;
+
+    public Book(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
